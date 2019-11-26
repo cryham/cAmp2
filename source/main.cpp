e27db235ba1d48204577a46d14d38e5a9cd10f26
cryham <cryham@gmail.com>
2019-11-26 23:30:48 +0100

    Util class for strings

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
 #include "FileSystem.h"
-#include "Playlist.h"
 #include "AppSFMLDraw.h"
 using namespace std;
 
@@ -11,30 +10,8 @@ int main()
     FileSystem::Init();
     cout << FileSystem::GetInfo();
 
-	//  laod Settings first
-	//------------------
-//	App* app = new App();
-//	app->set.Load();
-//	Settings& set = app->set;
-	
-    //LogFile::Create(FileSystem::Config());
-
-//	unique_ptr<App> app = make_unique<App>();
-//	app->Run();
-
 	unique_ptr<AppSFMLDraw> app = make_unique<AppSFMLDraw>();
-	app->Run();
+	bool ok = app->Run();
 	
-    //LoadSettings(FileSystem::Settings());
-
-    //LoadTexture(FileSystem::Data());
-
-	/*unique_ptr<Playlist> p = make_unique<Playlist>();
-    p->AddDir("../../../zm");
-
-    const auto& tracks = p->GetTracks();
-    for (const auto& trk: tracks)
-        cout << trk.GetName() << endl;*/
-	
-    return 0;
+    return ok ? EXIT_SUCCESS : EXIT_FAILURE;
 }
