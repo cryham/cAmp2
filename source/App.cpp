3e47d6e9679bc90a19e4b7d962d6497247e37321
cryham <cryham@gmail.com>
2019-12-29 09:12:16 +0100

    App UpdDim, layout and input vars from cAmp

--- source/App.cpp
+++ source/App.cpp
@@ -31,7 +31,7 @@ bool App::Init()
 	if (!set.Load())
 		return false;
 	
-	tmc.Load();
+	timeColors.Load();
 	
 	
 	//  audio
@@ -51,7 +51,7 @@ bool App::Init()
 		Log(trk.GetName());
 #endif
 
-	//  on thread ..
+	// todo: on thread ..
 	//pls->Update();
 	for (auto& trk: pls->GetTracks())
 		audio->GetTrkTime(trk);
@@ -70,4 +70,62 @@ bool App::Destroy()
 	audio->Destroy();
 	
 	return ok;
+}
+
+
+//  Update dims
+//------------------------------------------------------------------------
+void App::UpdDim(/*float rfrFq*/)
+{
+	view.xSize = set.xwSize;
+	view.ySize = set.ywSize;
+	view.visH = 96;  // par
+	
+	//Graphics::SetupStates(pDev);
+	if (view.visH > view.ySize)  view.visH = view.ySize;  //vis max
+	int Fy = view.Fy;
+
+	//  file info
+	yB_fi = 0;	yE_pl_btn = 20;  // btnsH |< >|
+
+	//  visualization
+	yB_vis = 16;  //yFvi = 64;
+	yE_vis = min(view.ySize, 28 + ((view.eVis!=viNone)? view.visH: 0) );
+	
+	//  pos bar
+	yB_pos = yE_vis;  yE_pos = yB_pos +9;
+	xW_pos = 0.03f;
+
+	//  pt = playlist tabs
+	yB_pt = yE_pos;  /*par+- +2 8pos*/
+	xW_pt_btn = 13;  // btnsW up,dn
+		xW_pt = (view.xSize - xW_pt_btn) / view.xNpt;
+		yH_pt = /*cfont[view.cfT]->*/Fy +2;
+	yE_pt = yB_pt + view.yNpt * yH_pt +4;
+
+	//  pl = playlist, tracks
+	yB_pli = yE_pt;
+	yB_pl = yB_pli + /*cfont[view.cfP]->*/Fy +2/*yHpli*/;
+	yE_pl = view.ySize - /*cfont[view.cfP]->*/Fy;
+	
+	yH_pl = yE_pl-1-yB_pl;  yL_pl = max(0, yH_pl / /*cfont[view.cfP]->*/Fy );
+		//delete[] Lxm;  Lxm = new int[yL_pl+4];
+	yE_pl = yL_pl * /*cfont[view.cfP]->*/Fy + yB_pl;
+	yH_pl = yE_pl-1-yB_pl;
+
+	/*xWplS = 14;*/  xW_plSm = 40;  //|sliderW, mW
+	xTm = view.xSize - view.xW_plS-3;  //|
+	//CList::Lin = yL_pl;
+
+	//if (rfrFq > 0.f)
+	//	tmD = int(rfrFq*1.f);	clear=1;
+	
+	// for..
+	//cfont[view.cfP]->StrCopy(" 0:00");  xt_ = cfont[view.cfP]->GetWidth();
+
+	//  gui checks pos
+	xB_gc = 106;  yB_gc = yB_pt+90;  xW_gc = 18;
+	xB_gck = 170;  yH_gc = /*cfont[view.cfG]->*/Fy+6; //[0
+	
+	//GuiReInit();
 }
--- source/App.h
+++ source/App.h
@@ -3,6 +3,7 @@
 #include "Settings.h"
 #include "AppLog.h"
 #include "TimeColors.h"
+#include "View.h"
 
 #include <SFML/Window/Event.hpp>  // input
 #include <memory>
@@ -18,36 +19,67 @@ public:
 
     virtual bool Run() = 0;
 
+protected:
 	
 	Settings set;
 	
-	TimeClrSets tmc;
+	TimeClrSets timeColors;
 	
 	std::unique_ptr<Audio> audio;
-
-protected:
 
 	std::unique_ptr<Playlist> pls;
 
 	//std::unique_ptr<Skin> skin;
 
+	ViewSet view;
 	
 	bool Init();
 	bool Destroy();
 
-	//  input
+	
+	//  keys
+	bool alt = false, ctrl = false, shift = false;
 	bool KeyDown(sf::Event::KeyEvent key);  // use own enum?
 	bool KeyUp(sf::Event::KeyEvent key);
 
-	//  mouse pos, button
-	int mx = 0, my = 0, mb = 0;
+	//  mouse pos
+	int xMpos=0,yMpos=0, xMold=0,yMold=0;  //, xWclick,yWclick
+	int xm=0,ym=0, xms=0,yms=0;  // pos,old
+	int xMs,yMs, yMFvi, xLs,yLs;
+
+	//  buttons, s old state
+	bool bLs=false,bL=false, bRs=false,bR=false, bMs=false,bM=false;
+	bool bLsl, bMInWnd;  float mti=0.f,mtiv=0.f;
+	
+	void Mouse();  // process, update
 	void Mouse(int x, int y)
-	{	mx = x;  my = y;  }
-	void Wheel(int d)
-	{	}
+	{	xMpos = x;  yMpos = y;  }
+	void Wheel(int d);
+	void MouseDown(int b);
+	void MouseUp(int b);
 	
-	bool bFps = false;  // show Fps
-	int yL_pl = 10;  // playlist view lines
 	
+	float dt = 0.1f;   // frame time, ms
+	bool bFps = false;   // show Fps
+	bool bDebug = false; // show debug text
+	int iTimeTest = 0;   // test pls time colors
+
+	//int yL_pl = 10;  // playlist view lines, auto set
+	//  dimensions
+	//  B = begin  E = end  W = width  H = height
+	float xW_pos=0;  // position marker
+	int yB_fi=0,  // file info
+		yB_vis=0,yE_vis=0,  // visualization
+		yB_pos=0,yE_pos=0, yE_pl_btn=0,  // posbar, btns
+		//  pls tabs, size, btns
+		yB_pt=0,yE_pt=0, xW_pt=0,yH_pt=0, xW_pt_btn=0,
+		// pls tracks  lines, |sliderW,mW, xTime
+		yB_pli=0, yB_pl=0,yE_pl=0, yH_pl=0,yL_pl=0, xW_plSm=0,xTm=0,
+		// gui checks
+		xB_gc=0,xB_gck=0,yB_gc=0, xW_gc=0,yH_gc=0;
+
+	void UpdDim(/*float rfrFq=-1.f*/);
+	
+	//  player
 	void Play();
 };
